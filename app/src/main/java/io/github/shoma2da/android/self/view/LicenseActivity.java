0b622a6252b0577cf4b1766744aa5363736d7876
shoma2da <marcos.shoichi.matsuda@gmail.com>
2015-10-31 18:15:47 +0900

    circleのエラー対応

--- app/src/main/java/io/github/shoma2da/android/self/view/LicenseActivity.java
+++ app/src/main/java/io/github/shoma2da/android/self/view/LicenseActivity.java
@@ -2,6 +2,7 @@ package io.github.shoma2da.android.self.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import io.github.shoma2da.android.self.R;
 
@@ -11,5 +12,20 @@ public class LicenseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_license);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //アクションバーの戻るを押したときの処理
+        if(id == android.R.id.home){
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
