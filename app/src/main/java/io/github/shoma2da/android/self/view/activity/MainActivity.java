071c8937f0d3ee6b79eec7394c5f46c3f6ad6f04
shoma2da <marcos.shoichi.matsuda@gmail.com>
2015-11-07 23:07:20 +0900

    設定画面への遷移を実装

--- app/src/main/java/io/github/shoma2da/android/self/view/activity/MainActivity.java
+++ app/src/main/java/io/github/shoma2da/android/self/view/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package io.github.shoma2da.android.self.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -56,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingActivity.class));
             return true;
         }
 
---
+++ app/src/main/java/io/github/shoma2da/android/self/view/activity/SettingActivity.java
@@ -0,0 +1,9 @@
+package io.github.shoma2da.android.self.view.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by shoma2da on 2015/11/07.
+ */
+public class SettingActivity extends Activity {
+}
