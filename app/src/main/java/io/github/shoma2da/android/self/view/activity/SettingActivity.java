a3d48d29d315fa3561b4ed2b49e8fa6cd71e3fc9
shoma2da <marcos.shoichi.matsuda@gmail.com>
2015-11-07 23:46:12 +0900

    設定画面の雛形を作成

--- app/src/main/java/io/github/shoma2da/android/self/view/activity/SettingActivity.java
+++ app/src/main/java/io/github/shoma2da/android/self/view/activity/SettingActivity.java
@@ -1,9 +1,51 @@
 package io.github.shoma2da.android.self.view.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import io.github.shoma2da.android.self.R;
 
 /**
  * Created by shoma2da on 2015/11/07.
  */
-public class SettingActivity extends Activity {
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("設定");
+
+        getFragmentManager().beginTransaction()
+                .replace(R.id.content, new SettingFragment())
+                .commit();
+    }
+
+    public static class SettingFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //アクションバーの戻るを押したときの処理
+        if(id == android.R.id.home){
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
